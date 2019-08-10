22909f84b83dbca9eaf7715eb7f33aae8075b96d
Sumeet Sharma <sksksharma0@gmail.com>
2019-08-10 22:24:30 +0530

    Login App using Firebase

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
   { path: 'contact', loadChildren: './pages/contact/contact.module#ContactPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
 ];
 
 @NgModule({
--- environments/environment.ts
+++ environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  	production: false,
+  	firebase: {
+	  	apiKey: "AIzaSyB8bOc4xX0efqz4zeDaeib3SmZFPTW3ZnM",
+		authDomain: "ionic-4-firebase-login-8033d.firebaseapp.com",
+		databaseURL: "https://ionic-4-firebase-login-8033d.firebaseio.com",
+		projectId: "ionic-4-firebase-login-8033d",
+		storageBucket: "",
+		messagingSenderId: "839446191457",
+		appId: "1:839446191457:web:9a1668165c439062"
+	}
 };
 
 /*
