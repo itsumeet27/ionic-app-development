04ed88b2c2ef51b42813f3fe5d1d995e6194e631
Sumeet Sharma <sksksharma0@gmail.com>
2019-08-10 19:52:51 +0530

    Adding About and Contact Pages

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
+  { path: 'contact', loadChildren: './pages/contact/contact.module#ContactPageModule' },
 ];
 
 @NgModule({
